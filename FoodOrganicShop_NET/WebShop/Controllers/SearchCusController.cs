652b1d6f8f73c6af3a5f005be2006f3a1169309b
VanToanVto <vantoanvto@gmail.com>
2023-11-16 15:58:55 +0700

    up

--- FoodOrganicShop_NET/WebShop/Controllers/SearchCusController.cs
+++ FoodOrganicShop_NET/WebShop/Controllers/SearchCusController.cs
@@ -6,9 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebShop.Models;
 using WebShop.ModelViews;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace WebShop.Controllers
 {
 	public class SearchCusController : Controller
