ee388ddda9b623cb4fe73c2183f1ef42c3dd9dac
VanToanVto <vantoanvto@gmail.com>
2023-11-16 15:56:40 +0700

    new

---
+++ FoodOrganicShop_NET/WebShop/wwwroot/js/time.js
@@ -0,0 +1,6 @@
+﻿var now = new Date();
+var year = now.getFullYear();
+var month = String(now.getMonth() + 1).padStart(2, '0');
+var day = String(now.getDate()+3).padStart(2, '0');
+var formattedDate = year + '/' + month + '/' + day;
+document.getElementById("time").setAttribute("data-countdown", formattedDate); 
