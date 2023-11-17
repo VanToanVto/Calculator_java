14fea369d26d024d73b9514edaef42c3ac976f4a
VanToanVto <vantoanvto@gmail.com>
2023-11-17 09:17:09 +0700

    full

--- FoodOrganicShop_NET/WebShop/Controllers/SearchCusController.cs
+++ FoodOrganicShop_NET/WebShop/Controllers/SearchCusController.cs
@@ -8,36 +8,20 @@ namespace WebShop.Controllers
 	public class SearchCusController : Controller
 	{
 		private readonly dbMarketsContext _context;
-
 		public SearchCusController(dbMarketsContext context)
 		{
 			_context = context;
 		}
-
 		[HttpPost]
-		[Route("/SearchCus/FindCusProduct/",Name ="ss")]		
-		
-			public IActionResult FindCusProduct(string keyword)
-			{
-				List<Product> ls = new List<Product>();
-				if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
-				{
-					return PartialView("ListProductsCusSearchPartial", null);
-				}
-				ls = _context.Products.AsNoTracking()
-									  .Where(x => x.ProductName.Contains(keyword))
-									  .OrderByDescending(x => x.ProductName)
-									  .ToList();
-				if (ls == null)
-				{
-					return PartialView("ListProductsCusSearchPartial", null);
-				}
-				else
-				{
-					return PartialView("ListProductsCusSearchPartial", ls);
-				}
-			}
-
+		[Route("/SearchCus/FindCusProduct/", Name = "ss")]
+		public IActionResult FindCusProduct(string keyword)
+		{
+			List<Product> ls = new List<Product>();
+			if (string.IsNullOrEmpty(keyword) || keyword.Length < 1) return PartialView("ListProductsCusSearchPartial", null);
+			ls = _context.Products.AsNoTracking().Where(x => x.ProductName.Contains(keyword))
+								  .OrderByDescending(x => x.ProductName).ToList();
+			if (ls == null) return PartialView("ListProductsCusSearchPartial", null);
+			else return PartialView("ListProductsCusSearchPartial", ls);
 		}
-	
+	}
 }
